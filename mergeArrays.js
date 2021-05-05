c713f5e44573b03032d67489b480be9b4aaaf8ac
Ahmed Alani <s3leha@gmail.com>
2021-05-05 13:36:22 -0700

    added a file mergeArrays.js, will implement function later

---
+++ mergeArrays.js
@@ -0,0 +1,11 @@
+const mergeArrays = (arr1, n1, arr2, n2) => {
+  return "✅";
+};
+
+// tests
+let nums1 = [1, 2, 3, 0, 0, 0];
+let m = 3;
+let nums2 = [2, 5, 6];
+let n = 3;
+
+console.log(mergeArrays(nums1, m, nums2, n));
