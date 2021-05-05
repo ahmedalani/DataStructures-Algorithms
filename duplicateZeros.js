d4c7c3a859b1924a18d5b5293e8c1af7a1f2ec5f
Ahmed Alani <s3leha@gmail.com>
2021-05-05 13:29:17 -0700

    duplicateZeros function is working and passed all tests

--- duplicateZeros.js
+++ duplicateZeros.js
@@ -1,6 +1,18 @@
-const duplicateZeros = (arr) => {};
+const duplicateZeros = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    const num = arr[i];
+    if (num === 0) {
+      // 0 in the middle is indicated how many items we want removed,
+      // 0 at the end is the item we want to be inserted into the array
+      arr.splice(i, 0, 0);
+      arr.pop();
+      i++;
+    }
+  }
+};
 
 // tests
 let arr1 = [1, 0, 2, 3, 0, 4, 5, 0];
+console.log("first: ", arr1);
 duplicateZeros(arr1);
-console.log(arr1);
+console.log("second: ", arr1);
