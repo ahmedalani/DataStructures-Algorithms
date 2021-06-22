15fe5c6213dd216d6e349491c1e901355bca6eb1
Ahmed Alani <s3leha@gmail.com>
2021-06-22 14:25:24 +0300

    added comments

--- replaceElements.js
+++ replaceElements.js
@@ -1,6 +1,7 @@
 const replaceElements = (arr) => {
   let newArr = [];
   newArr[arr.length - 1] = -1;
+  // loop over arry from the right side copare 2 itmes at a time and put biggest one in newArr
   for (let i = arr.length - 2; i >= 0; i--) {
     newArr[i] = Math.max(newArr[i + 1], arr[i + 1]);
   }
