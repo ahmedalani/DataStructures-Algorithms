1a8862c297d6b47df55ec1a4859506e8174859b4
Ahmed Alani <s3leha@gmail.com>
2021-06-22 14:24:35 +0300

    implemented the replaceElements function, works well, passed all tests

--- replaceElements.js
+++ replaceElements.js
@@ -1,1 +1,10 @@
+const replaceElements = (arr) => {
+  let newArr = [];
+  newArr[arr.length - 1] = -1;
+  for (let i = arr.length - 2; i >= 0; i--) {
+    newArr[i] = Math.max(newArr[i + 1], arr[i + 1]);
+  }
+  return newArr;
+};
 
+console.log(replaceElements([17, 18, 5, 4, 6, 1])); // [18,6,6,6,1,-1]
