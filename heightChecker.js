b5e2b121d63fdb5e83444e2d6ff977441fa06916
Ahmed Alani <s3leha@gmail.com>
2021-07-24 12:46:17 +0300

    heightChecker function implemented, passed all tests

--- heightChecker.js
+++ heightChecker.js
@@ -1,12 +1,23 @@
 const heightChecker = (heights) => {
-  return heights;
+  let output = 0;
+  const expected = [...heights].sort((a, b) => a - b);
+  heights.forEach((e, i) => {
+    if (e !== expected[i]) {
+      output++;
+    }
+  });
+  return output;
 };
 
 //Test
 const arr1 = [1, 1, 4, 2, 1, 3];
 const arr2 = [5, 1, 2, 3, 4];
 const arr3 = [1, 2, 3, 4, 5];
+const arr4 = [
+  10, 6, 6, 10, 10, 9, 8, 8, 3, 3, 8, 2, 1, 5, 1, 9, 5, 2, 7, 4, 7, 7,
+];
 
 console.log(heightChecker(arr1)); //3
 console.log(heightChecker(arr2)); //5
 console.log(heightChecker(arr3)); //0
+console.log(heightChecker(arr4)); //22
