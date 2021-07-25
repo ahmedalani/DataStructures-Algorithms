1260677e8e67c0cc3d98c376a820c9a59ef7f935
Ahmed Alani <s3leha@gmail.com>
2021-07-25 12:11:44 +0300

    new file added, function implementation in progress

---
+++ thirdMax.js
@@ -0,0 +1,12 @@
+const thirdMax = (nums) => {
+  return nums;
+};
+
+//Test
+const arr1 = [3, 2, 1];
+const arr2 = [1, 2];
+const arr3 = [2, 2, 3, 1];
+
+console.log(thirdMax(arr1));
+console.log(thirdMax(arr2));
+console.log(thirdMax(arr3));
