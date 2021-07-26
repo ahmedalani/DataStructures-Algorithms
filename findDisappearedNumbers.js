30fb6b56be4e9d7d965df7460421b9e9511b18c7
Ahmed Alani <s3leha@gmail.com>
2021-07-26 12:50:59 +0300

    findDisappearedNumbers function added, work in progress

---
+++ findDisappearedNumbers.js
@@ -0,0 +1,10 @@
+const findDisappearedNumbers = (nums) => {
+  return nums;
+};
+
+// Tests
+let arr1 = [4, 3, 2, 7, 8, 2, 3, 1];
+let arr2 = [1, 1];
+
+console.log(findDisappearedNumbers(arr1)); // [5,6]
+console.log(findDisappearedNumbers(arr2)); // [2]
