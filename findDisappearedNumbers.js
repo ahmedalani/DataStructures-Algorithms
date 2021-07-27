b79bd688f4530926feb8f53c1af43788fb658603
Ahmed Alani <s3leha@gmail.com>
2021-07-27 13:11:29 +0300

    findDisappearedNumbers function passed all test, great runtime result. final commit

--- findDisappearedNumbers.js
+++ findDisappearedNumbers.js
@@ -1,10 +1,22 @@
 const findDisappearedNumbers = (nums) => {
-  return nums;
+  let result = [];
+  const sortedNums = [...nums].sort((a, b) => a - b);
+  const sortedNumsNoDuplicates = [...new Set(sortedNums)];
+  let counter = 1;
+  for (let i = 0; i < nums.length; i++) {
+    const element = sortedNumsNoDuplicates[i];
+
+    if (element !== counter && counter <= nums.length) {
+      result.push(counter);
+      i--;
+    }
+    counter++;
+  }
+  return result;
 };
 
 // Tests
 let arr1 = [4, 3, 2, 7, 8, 2, 3, 1];
 let arr2 = [1, 1];
-
 console.log(findDisappearedNumbers(arr1)); // [5,6]
 console.log(findDisappearedNumbers(arr2)); // [2]
