45b1c863f8522d0d2bd81d9bb587c8f5da91efdf
Ahmed Alani <s3leha@gmail.com>
2022-06-14 19:30:27 -0700

    is num a palindrome, solved

--- otherProbs/palindromeNumber.js
+++ otherProbs/palindromeNumber.js
@@ -1,3 +1,16 @@
 // given an integer x return true if int is palindrome 
 // palindrome int reads the same backwards ex: 121 = true ; 123 = false
 
+const isPalindrome = function(x) {
+  if (isNaN(x)) {
+    return false;
+  }
+  const reverseX = x.toString().split('').reverse().join('');
+  return x === Number(reverseX);
+};
+
+
+// console.log(isPalindrome('a')) // enter valid num
+console.log(isPalindrome(121)) // true
+console.log(isPalindrome(123)) // false
+console.log(isPalindrome(-121)) // false
